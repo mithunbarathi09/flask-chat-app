5d1a64dc0ecc43547cc733c5e3043f535e411321
Evan Luo <contact@ohevan.com>
2023-06-20 19:33:06 -0400

    front-end: optimized

---
+++ static/js/room.js
@@ -0,0 +1,35 @@
+function stringToColor(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const hue = hash % 360;
+  const saturation = 70; // Saturation percentage for pastel colors
+  const lightness = 40; // Lightness percentage for good readability on white background
+
+  return hslToHex(hue, saturation, lightness);
+}
+
+function hslToHex(h, s, l) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0");
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+const messageInput = document.getElementById("message");
+
+messageInput.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Trigger the button element with a click
+    document.getElementById("send_btn").click();
+  }
+});
