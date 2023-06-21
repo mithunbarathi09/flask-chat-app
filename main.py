6f369d0a77c05abf01500484e12146fe0319cb10
Mithun <mithunbarathi.qc@gmail.com>
2023-06-21 11:09:13 -0400

    Made it work on local wifi

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ def disconnect():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host="0.0.0.0")
